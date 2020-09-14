aca25faa83785dd52bfc4cfb36563933ede6ec82
Taimoor khan <khant8k83@gmail.com>
2020-09-14 21:17:23 +0500

    Spreading Nested Objects and changing challenge done

--- assignment_02/script.js
+++ assignment_02/script.js
@@ -57,3 +57,26 @@ const stdObj = {
 // console.log(Movie, Silver, Comfort);
 
 //======== Spreading Objects and Changing them ========
+
+// let nestedObj = {
+//   movieName: "Justice League (Snyder Cut)",
+//   premierDate: "20 October 2020",
+//   TickectsAvailable: 100,
+//   pricing: {
+//     Gold: 1700,
+//     Business: 1000,
+//     Luxury: 800,
+//     Standard: 500,
+//   },
+// };
+
+// let newRates = {
+//   ...nestedObj,
+//   pricing: {
+//     ...nestedObj.pricing,
+//     Standard: 600,
+//   },
+// };
+
+// console.log("Old Price: " + nestedObj.pricing.Standard);
+// console.log("New Price: " + newRates.pricing.Standard);
